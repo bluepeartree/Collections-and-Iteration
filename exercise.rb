5040dab2295abfefc2006b39c48ca83b465ec0d8
bluepeartree <sherrie.sebastian@gmail.com>
2017-11-01 16:47:36 -0400

    exercise 1

---
+++ exercise.rb
@@ -0,0 +1,21 @@
+colours = ["blue", "lavender", "red"]
+ages = [32, 30, 33, 31]
+flip_results = ["heads", "tails", "heads", "tails", "heads"]
+artists = ["Franz Ferdinand", "Fleet Foxes", "EXO"]
+colours_s = [:blue, :lavender, :red]
+
+dictionary = {:fast=> "moving or capable of moving at high speed", :slow => "educe one's speed or the speed of a vehicle or process", :angry => "having a strong feeling of or showing annoyance, displeasure, or hostility; full of anger"}
+movies = {:star_wars=>1977, :avengers=>2012}
+cities= {:toronto=>2.81, :mumbai=>18.41, :new_york=>8.54}
+ages_name ={:joshua=>29, :stephanie =>34, :rini=>31}
+
+
+#Exercise 1#
+
+puts flip_results
+puts colours[0]
+puts ages.sort
+
+ages << 0
+
+puts movies[:avengers]
